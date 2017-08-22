ee803e91560cf0663224dc00aeaa263697e696bc
Komputer <Komputer@DESKTOP-7D7M4H3>
2017-08-22 21:55:08 +0200

    poprawiona struktura
    wydzielono folder na pliki z danymi

--- main folder/js/40lvl.js
+++ main folder/js/40lvl.js
@@ -1,7 +1,7 @@
 //main controller for angular
 var app = angular.module('main', []);
 app.controller('mainCtrl', function($scope, $http, $sce) {
-	$http.get("components/40lvl.txt").then(function(response) {
+	$http.get("datafiles/40lvl.txt").then(function(response) {
 		$scope.playersTable = response.data.split('\n');
 	});
 });
--- main folder/js/media.js
+++ main folder/js/media.js
@@ -4,7 +4,7 @@ app.controller('mainCtrl', function($scope, $http, $sce) {
 	$scope.trustSrc = function(src) {
 		return $sce.trustAsResourceUrl(src.replace("watch?v=", "embed/"));
 	}
-	$http.get("components/movies.txt").then(function(response) {
+	$http.get("datafiles/movies.txt").then(function(response) {
 		$scope.movies = response.data.split('\n');
 	});
 });
