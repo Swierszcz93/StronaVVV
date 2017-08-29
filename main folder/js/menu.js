580fbc173f4953ccc6209feb903fa12f4fee8af5
Komputer <Komputer@192.168.0.200>
2017-08-29 20:59:45 +0200

    usunieto zbedne skrypty dla menu

--- main folder/js/menu.js
+++
@@ -1,9 +0,0 @@
-function openMobileMenu() {
-	//alert("jestem");
-	var menu = document.getElementById("menu");
-	if (menu.className === "menu") {
-		menu.className += " responsive";
-	} else {
-		menu.className = "menu";
-	}
-}
