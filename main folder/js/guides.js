00d5364e3715bb3f53a08c0464e3c9b08142323f
Komputer <Komputer@192.168.0.200>
2017-08-29 22:22:08 +0200

    ladowanie poradnikow z pliku do listy

--- main folder/js/guides.js
+++ main folder/js/guides.js
@@ -1,4 +1,23 @@
 //main controller for angular
 var app = angular.module('main', []);
 app.controller('mainCtrl', function($scope, $http, $sce) {
+	$http.get("datafiles/guides.txt").then(function(response) {
+		var rawGuidesTable = response.data.split('\n');
+		$scope.guidesTable = [];
+		for(var i=0;i<rawGuidesTable.length;i++){
+			$scope.guidesTable.push(rawGuidesTable[i].split(';'));
+		}
+	});
 });
+
+function loadSelectedGuide(){
+	log('loadSelectedGuide');
+}
+
+function removePreviousContent(){
+	
+}
+
+function fillWithNewContent(){
+	
+}
